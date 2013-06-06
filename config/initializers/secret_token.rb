49a9908cd8cf6cea9b926668aeee569e7439b732
cbreitenstein <colleenbone@gmail.com>
2013-06-05 22:10:35 -0700

    creating barntalk application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Barntalk::Application.config.secret_token = '2c1356812110f9b6c068d6bb996e5e3e98a905096dec00932e7be123d0d4d2957f00d0dcfbe66f1014e9da45eca9cd60b7a1d1ada489264c526e99de875e389f'
